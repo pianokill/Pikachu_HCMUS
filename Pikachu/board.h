bec70b00b7e709b28dcda9d1b993930cf4c67d3a
phgnm <119720771+phgnm@users.noreply.github.com>
2023-03-26 00:21:04 +0700

    merged finding path algo

--- Pikachu/board.h
+++ Pikachu/board.h
@@ -1,9 +1,12 @@
 #pragma once
 #include <iostream>
 #include <algorithm>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
+void swap_2int(int& a, int& b);
 struct Point {
 	int x;
 	int y;
@@ -12,12 +15,29 @@ struct Node {
 	Point data;
 	Node* pNext;
 };
+void addHead(Node*& pHead, Point data);
+void addTail(Node*& pHead, Point data);
+void insertTail(Node*& pHead, Node* test);
+void removeHead(Node*& pHead);
+void removeAll(Node*& pHead);
+void printList(Node* pHead);
+Node* reverseList(Node* pHead);
 struct board
 {
 	int** isOktogo; // To check whether a cell is available on the board
 	char** letters; // The character that the players will see on the board
 	int difficulty; // Difficulty of the game, which will be 1 (Easy), 2 (Medium), 3 (Hard)
 	const pair<int, int> size[3] = { {3,4},{6,8},{10,12} }; // For each difficulty, a respective size of the board will be displayed
-	void init(int difficulty); // Initialize the board based on the chosen difficulty
+	void init(); // Initialize the board based on the chosen difficulty
 	void destroy_board(); // Delete the board after the game ends
+	bool matching_I(Point, Point);
+	bool matching_L(int, Point, Point);
+	int matching_Z_U(int, int, Point, Point);
+	int matching_check(int, int, Point, Point);
+	Node* path_I(Point, Point);
+	Node* path_L(int, Point, Point);
+	Node* path_U_Z(int, int, Point, Point);
+	Node* path_finding(int, int, int, Point, Point);
+	bool automatically_finding(int, int, Point&, Point&);
+	int FindScore();
 };
