b98f56cdc6553ed6872f4f56aa071fd3eed6e7ee
Nguyen Thanh Tuan Kiet <124020170+pianokill@users.noreply.github.com>
2023-03-30 11:43:36 +0700

    Delete controller.h

--- Pikachu/controller.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "game.h"
-#include "menu.h"
-#include <mmsystem.h>
-#pragma comment(lib, "winmm.lib")
-
-void controlGame();
-void playingGame(menu& a, int diff, int& pairs, int& fin_score);
