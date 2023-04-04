4a73897b5e9967a46b3a23017c8bd040357cb6c1
phgnm <119720771+phgnm@users.noreply.github.com>
2023-04-04 15:44:09 +0700

    added info board

--- Pikachu/controller.h
+++ Pikachu/controller.h
@@ -1,13 +1,11 @@
 #pragma once
-
 #include "game.h"
 #include "menu.h"
 #include "account.h"
 #include <mmsystem.h>
-#include <ctime>
 #pragma comment(lib, "winmm.lib")
 
-void controlGame(Account list_acc[], int& acc_num, finish F[], int& fin); 
-void playingGame(Account acc[], int acc_pos, int acc_num, game& b, bool loaded, finish F[], int &fin); 
+void controlGame(Account list_acc[], int& acc_num, finish F[], int& fin);
+void playingGame(Account acc[], int acc_pos, int acc_num, game& b, bool loaded, finish F[], int& fin, menu a);
 void onRoundTheme(int diff); //Turn on the music theme for each difficulty board game
 void offRoundTheme(int diff); //Turn off the music theme for each difficulty board game
