91ad645179ac51576ecf981dd2df5ece77bc9a10
phgnm <119720771+phgnm@users.noreply.github.com>
2023-04-11 20:51:34 +0700

    the game is basically done

--- Pikachu/account.h
+++ Pikachu/account.h
@@ -1,14 +1,16 @@
 #pragma once
 #include "game.h"
-#include "iomanip"
-#define BOARDSIZE 1000
-#define NAME 50
-#define PASS 50
+#include "menu.h"
+#include <iomanip>
+#include <limits>
+
+const int BOARDSIZE = 1000;
+const int PASS = 50;
 
 struct Account
 {
     int file_number; //Number of saved file
-    char name[NAME]; 
+    char name[NAME];
     char pass[PASS];
     filesave saves[3]; //Each account has maximum 3 file saves
 };
@@ -18,8 +20,8 @@ void saving_map(Account& acc, const game& Game); //Saving the playing game to us
 void xor_filesave(filesave& save, int mask);
 void xor_account(Account& acc, int mask);
 bool printing_account(Account acc[], int acc_number, int mask); //Printing the list of accounts to account_log.bin
-bool reading_account(Account acc[], int &acc_number, int mask); //Loading the list of accounts from account_log.bin
-int loading_account(Account acc[], int &acc_number, Account guest); //Loading the account in the list, if not in the list => a new account added to list
-bool Changeaccount_name(Account list_acc[], int acc_num, int pos);
-bool Changeaccount_password(Account list_acc[], int acc_num, int pos);
-bool Changeaccount_filesave(Account list_acc[], int acc_num, int pos);
+bool reading_account(Account acc[], int& acc_number, int mask); //Loading the list of accounts from account_log.bin
+int loading_account(Account acc[], int& acc_number, Account guest); //Loading the account in the list, if not in the list => a new account added to list
+void Changeaccount_name(Account list_acc[], int acc_num, int pos);
+void Changeaccount_password(Account list_acc[], int acc_num, int pos);
+void Changeaccount_filesave(Account list_acc[], int acc_num, int pos, menu mn);
