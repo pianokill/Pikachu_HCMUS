e1265dfa4885adabd7853d3511a40c99363a2177
phgnm <119720771+phgnm@users.noreply.github.com>
2023-04-11 21:02:01 +0700

    the game is basically done

--- Pikachu/main.cpp
+++ Pikachu/main.cpp
@@ -7,10 +7,6 @@ int main()
 	Account list_acc[20]; //Number of account to manage 
 	int acc_num = 0;
 	reading_account(list_acc, acc_num, 777);
-	for (int i = 0; i < acc_num; i++)
-	{
-		cout << i << ": " << list_acc[i].name << " " << list_acc[i].pass << endl;
-	}
 	finish Leaderboard[20]; //Number of finished game to be in the leaderboard.bin => get top 5 of each size only
 	int fin = 0;
 	readingLeaderboard(Leaderboard, fin, 0x12345678);
